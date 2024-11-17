cb026f1d4c10b43a4f721f15f4ff2dc6c7398bb4
yashkulshrestha1102 <113239931+yashkulshrestha1102@users.noreply.github.com>
2024-11-17 15:33:10 +0000

    ok

---
+++ 01_basics/variables.js
@@ -0,0 +1,20 @@
+const accountId = 12345
+let accountEmail = "yashkul123@gmail.com"
+var accountPassword = "54321"
+accountCity = "Ghaziabad"
+let hhh;
+// hhh ka hoga undefined jabtak use kio value assign nhi hogi
+
+
+// accountId=9 // not allowed kyu ki const mai ek bar value assign karne pe change nhi karte 
+
+
+
+accountEmail = "gh@gmail.com"
+accountPassword="465465"
+accountCity = "bengaluru"
+
+
+console.log(accountId)
+
+console.table([accountId,accountEmail,accountPassword,accountCity,hhh])
